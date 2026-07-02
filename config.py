6deb74d63c4c0def7446dfb4ec6352d309928ff3
dalerik920-boop <dalerik920@gmail.com>
2026-07-02 14:30:34 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+# Telegram токен от BotFather
+BOT_TOKEN = "8940336472:AAF9hvwB44HOJDSdIXYA_Trt6cPiapmPzIo"
+
+# Groq API ключ (gsk_...)
+GROQ_API_KEY = "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"
+CREATOR_NAME = "Хикматулаев Далер"
+CREATOR_YEAR = "2012"
+CREATOR_DATE = "2 июля 2026"
